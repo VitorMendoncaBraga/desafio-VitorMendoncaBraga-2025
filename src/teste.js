5bfa571c0fe8739f2c245f59334927349714db0b
Vitor Mendonça Braga <148807690+VitorMendoncaBraga@users.noreply.github.com>
2025-09-07 12:04:44 -0300

    Delete src/teste.js

--- src/teste.js
+++
@@ -1,7 +0,0 @@
-import { AbrigoAnimais } from "./abrigo-animais.js";
-
-const abrigoAnimais = new AbrigoAnimais()
-
-const resultado = abrigoAnimais.encontraPessoas('BOLA,LASER',
-      'BOLA,NOVELO,RATO,LASER', 'Mimi,Fofo,Rex,Bola')
-console.log(resultado)
